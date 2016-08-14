e52023e1c3e0aa9441c91e42920d44e78dfdf825
Gerald Ang <geraldangw@gmail.com>
2016-08-14 21:10:36 +0800

    adjusted test to pass recent changes

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -23,22 +23,7 @@ describe(" GA Profile Express Server API", function() {
                   email: 'geraldang@gmail.com',
                   contactnum: '+6598800333',
                   dateofbirth: '29th July 1983',
-                  image: 'https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/11666055_10153973280630639_4839601104696918395_n.jpg?oh=cdd71ed8cfc452b2631959250e50be65&oe=5813EA06'
-                }, done);
-      });
-  });
-  describe("should return information json on get /links", function() {
-      it("returns status code 200", function(done) {
-        supertest(app)
-          .get('/links')
-          .expect('Content-Type', /json/)
-          .expect(200, done);
-      });
-
-      it("returns a correct json format", function(done) {
-        supertest(app)
-          .get('/links')
-          .expect({
+                  image: 'https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/11666055_10153973280630639_4839601104696918395_n.jpg?oh=cdd71ed8cfc452b2631959250e50be65&oe=5813EA06',
                   facebook: 'https://www.facebook.com/gerald.ang.982',
                   linkedin: 'https://sg.linkedin.com/in/gerald-ang-8344463a',
                   emailme: 'mailto:geraldangw@gmail.com'
