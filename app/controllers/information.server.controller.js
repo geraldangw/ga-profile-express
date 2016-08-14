670de31ca6cdaeea1afaa4181f187bef6df76d0e
Gerald Ang <geraldangw@gmail.com>
2016-08-14 21:09:23 +0800

    combined link information on information server

--- app/controllers/information.server.controller.js
+++ app/controllers/information.server.controller.js
@@ -7,7 +7,10 @@ module.exports = {
         email: 'geraldang@gmail.com',
         contactnum: '+6598800333',
         dateofbirth: '29th July 1983',
-        image: 'https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/11666055_10153973280630639_4839601104696918395_n.jpg?oh=cdd71ed8cfc452b2631959250e50be65&oe=5813EA06'
+        image: 'https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/11666055_10153973280630639_4839601104696918395_n.jpg?oh=cdd71ed8cfc452b2631959250e50be65&oe=5813EA06',
+        facebook: 'https://www.facebook.com/gerald.ang.982',
+        linkedin: 'https://sg.linkedin.com/in/gerald-ang-8344463a',
+        emailme: 'mailto:geraldangw@gmail.com'
       }
     );
   }
--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -6,7 +6,6 @@ module.exports = function(app) {
 
 var staticPageController = require('../controllers/staticpage.server.controller');
 var informationController = require('../controllers/information.server.controller');
-var linksController = require('../controllers/links.server.controller');
 var projectsController = require('../controllers/projects.server.controller');
 // requires the controller of static pages.
 
@@ -22,6 +21,5 @@ app.get('/', staticPageController.renderHome);
 app.get('/about', staticPageController.renderAbout);
 app.get('/contact', staticPageController.renderContact);
 app.get('/information', informationController.index);
-app.get('/links', linksController.index);
 app.get('/projects', projectsController.index);
 };
