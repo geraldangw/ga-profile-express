4294d5afd00a1f9e89baf395900252b3ba5b1fd2
Gerald Ang <geraldangw@gmail.com>
2016-08-17 12:16:09 +0800

    changed mongolab deployment

--- config/env/production.js
+++ config/env/production.js
@@ -1,3 +1,3 @@
 module.exports = {
-  db: 'mongodb://geraldangw:admin1234@ds161295.mlab.com:61295/heroku_zkzdp50c', // diff url for heroku
+  db: process.env.PROD_MONGODB, // diff url for heroku
 };
